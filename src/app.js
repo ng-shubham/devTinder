244e17b40d648fca1d12a8a1905289f80ab36d22
ng-shubham <ngshubham01@gmail.com>
2025-02-26 17:02:48 +0530

    Few routes created

--- src/app.js
+++ src/app.js
@@ -2,14 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("Hello from Server!!")
-})
-app.use("/hello", (req, res) => {
-    res.send("Hello ")
+//Get API call
+app.get("/user", (req, res) => {
+    console.log(req.query);     //Read the query (/user?userid=101&status=Done)
+    console.log(req.params);    //Read the params (/user/:userID)
+    res.send({ fName: "Shubham", lname: "Ambhore"})
 })
 
+//Post API call
+app.post("/user", (req, res) => { 
+    res.send("Data successfully saved!")
+})
+
+// Delete API Call
+app.delete("/user", (req, res) => { 
+    res.send("Data successfully Deleted!")
+})  
+
 app.listen(5555, () => {
-    console.log("Server Listening on port 5555");
-    
+    console.log("Server Listening on port 5555"); 
 })
